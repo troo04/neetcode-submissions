692bdfc1474dfd439b7129413195d5b1f612bfe4
Thiru Sethuraman <74469020+troo04@users.noreply.github.com>
2026-09-05 11:53:12 -0500

    Time: 94 ms (22.66%) | Memory: 22.3 MB (6.59%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -15,7 +15,7 @@ class Solution:
             if (node[1], node[2]) in best and best[(node[1], node[2])] <= node[0]:
                 continue
 
-            best[(node[1], node[2])] = cost
+            best[(node[1], node[2])] = node[0]
 
             if node[1] == dst:
                 return node[0]
@@ -24,7 +24,10 @@ class Solution:
                 continue
 
             for neighbor in adj_map[node[1]]:
-                    if neighbor not in best or node[0] + neighbor[1] < best[neighbor]:
-                        heapq.heappush(queue, (node[0] + neighbor[1], neighbor[0], node[2] + 1))
+                new_state = (neighbor[0], node[2] + 1)
+                new_cost = node[0] + neighbor[1]
+
+                if new_state not in best or new_cost < best[new_state]:
+                    heapq.heappush(queue, (new_cost, neighbor[0], node[2] + 1))
         
         return -1
