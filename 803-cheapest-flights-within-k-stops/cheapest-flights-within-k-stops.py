d5c669e888d41c23e6b2cf37f036b46337840739
Thiru Sethuraman <74469020+troo04@users.noreply.github.com>
2026-09-05 11:42:40 -0500

    Time: 15 ms (53.92%) | Memory: 25.8 MB (5.95%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj_map = defaultdict(list)
+        
+        for flight in flights:
+            s, dest, cost = flight
+            adj_map[s].append((dest, cost))
+        
+        best = {}
+
+        queue = [(0, src, 0)]
+        while queue:
+            node = heapq.heappop(queue) 
+            
+            if (node[1], node[2]) in best and best[(node[1], node[2])] <= node[0]:
+                continue
+
+            best[(node[1], node[2])] = cost
+
+            if node[1] == dst:
+                return node[0]
+            
+            if node[2] > k:
+                continue
+
+            for neighbor in adj_map[node[1]]:
+                    if neighbor not in best or node[0] + neighbor[1] < best[neighbor]:
+                        heapq.heappush(queue, (node[0] + neighbor[1], neighbor[0], node[2] + 1))
+        
+        return -1
