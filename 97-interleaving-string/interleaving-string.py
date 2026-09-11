3a0f031dad4a5f7a63bdeb788eb851795ed01927
Thiru Sethuraman <74469020+troo04@users.noreply.github.com>
2026-09-11 11:51:44 -0500

    Time: 51 ms (58.73%) | Memory: 20.1 MB (29.28%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        cache = {}
+        def dp(i, j, k):
+            if (i, j, k) in cache:
+                return cache[(i, j, k)]
+            
+            if k == len(s3):
+                return True
+            
+            res = False
+            if i < len(s1) and s1[i] == s3[k]:
+                res = res or dp(i + 1, j, k + 1)
+            if j < len(s2) and s2[j] == s3[k]:
+                res = res or dp(i, j + 1, k + 1)
+            
+            cache[(i, j, k)] = res
+            return cache[(i, j, k)]
+            
+        return dp(0, 0, 0)
